49cf2560f72497d9fae1b9784fe93a305cdee28a
alliekatautomations <mike@alliekatautomations.com>
2026-03-19 14:41:07 -0500

    server.js

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/diagnose', (req, res) => {
+    const {vin, code } = req.body;
+
+    res.json){
+        diagnosis: Allie Analysis:
+Vehicle: ${vin}
+code: ${code}
+
+Likely Causes:
+-Sensor Failure
+-Wiring Failure
+-Connector Problem
+
+Next Step:
+Check Power/ground and inspect harness.'
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send("Allie-kat backend running");
+});
+
+app.listen(3000, () => console.log("Server running"));
