f3fee5c43ad7a8babe403d7daa08a6081c76a60e
alliekatautomations <mike@alliekatautomations.com>
2026-03-20 16:46:48 -0500

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://julpheuumolnwkthazdj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1bHBoZXV1bW9sbndrdGhhemRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzc5ODYsImV4cCI6MjA4OTYxMzk4Nn0.i3jI-PjdAUPnbgVn_EXctr0-F158Gbp-r6icrEdvOGM'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
